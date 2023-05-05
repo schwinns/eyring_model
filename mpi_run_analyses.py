b00497e22f1986952301b604c68fcfcd1305959b
Nate Schwindt <nathanael.schwindt@colorado.edu>
2023-05-05 15:26:38 -0600

    parallelize convergence testing

--- mpi_run_analyses.py
+++ mpi_run_analyses.py
@@ -552,50 +552,107 @@ if estimate_dH_dS_spread:
 
 if test_path_convergence:
 
-    replicates = 1000
+    # run this on all available processors
+    from mpi4py import MPI
 
-    # Increasing number of paths to see how effective barrier changes (when it stabilizes)
-    n_paths = np.array([50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,
+    def calculate_barriers_permeability(N):
+
+        # Inputs for testing
+        T = 300
+        barrier = 10
+        sigma = barrier / 3
+
+        n_jumps = 50
+
+        dist = 'exponential'
+        params = {'beta' : barrier}
+
+        model = EyringModel(T=T)
+        for n in range(N):
+            model.add_Path(dist=dist, dist_params=params, n_jumps=n_jumps, lam=10) # 10 Angstrom jump lengths over 50
+
+        return model.calculate_effective_barrier(), model.calculate_permeability()
+
+    ########### DISTRIBUTE THE WORK ACROSS PROCESSORS ##########
+
+    comm = MPI.COMM_WORLD
+    nprocs = comm.Get_size()
+    rank = comm.Get_rank()
+
+    if rank == 0:
+        fig1, ax1 = plt.subplots(1,1)
+        fig2, ax2 = plt.subplots(1,1)
+        
+        # Increasing number of paths to see how effective barrier changes (when it stabilizes)
+        n_paths = np.array([50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,
                         1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,
                         3000,3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,4100,4200,4300,
-                        4400,4500,4600,4700,4800,4900,5000])
+                        4400,4500,4600,4700,4800,4900,5000], dtype='i')
+        
+        # split up n_paths for each proc
+        ave, res = divmod(n_paths.size, nprocs)
+        count = np.array([ave + 1 if p < res else ave for p in range(nprocs)], dtype='i')
 
-    # Inputs for testing
-    T = 300
-    barrier = 10
-    sigma = barrier / 3
+        # save the indices where each processor starts
+        displ = np.array([sum(count[:p]) for p in range(nprocs)])
 
-    n_jumps = 50
+        # save to calculate average across realizations
+        avg_barrier = np.zeros(len(n_paths))
+        avg_permeability = np.zeros(len(n_paths))
 
-    dist = 'normal'
-    params = {'mu' : barrier, 'sigma' : sigma}
+    else:
+        # initialize n_paths, count, displ on workers
+        n_paths = None
+        count = np.zeros(nprocs, dtype='i')
+        displ = None
 
-    effective_barriers = np.zeros(len(n_paths))
-    permeabilities = np.zeros(len(n_paths))
+    # send count to all processors
+    comm.Bcast(count, root=0)
 
-    fig1, ax1 = plt.subplots(1,1)
-    fig2, ax2 = plt.subplots(1,1)
+    # initialize my_paths on all processors
+    my_paths = np.zeros(count[rank], dtype='i')
 
-    for r in range(replicates):
+    # break the array up and send to processors
+    comm.Scatterv([n_paths, count, displ, MPI.INT], my_paths, root=0)
 
-        print(f'Realization: {r+1}')
-        for i,N in tqdm(enumerate(n_paths)):
+    # calculate effective barriers and permeabilities on each processor
+    effective_barriers = np.zeros(len(my_paths))
+    permeabilities = np.zeros(len(my_paths))
 
-            model = EyringModel(T=T)
-            for n in range(N):
-                model.add_Path(dist=dist, dist_params=params, n_jumps=n_jumps, lam=10) # 10 Angstrom jump lengths over 50 jumps = 500 Angstrom thickness
-                
-            effective_barriers[i] = model.calculate_effective_barrier()
-            permeabilities[i] = model.calculate_permeability()
+    ########## RUN A BUNCH OF REALIZATIONS ##########
 
-    
-        ax1.plot(n_paths, effective_barriers + R*T*np.log(n_paths))
-        ax2.plot(n_paths, permeabilities / n_paths)
+    realizations = 100
+    for r in range(realizations):
 
-  
-    ax1.xlabel('number of paths')
-    ax1.ylabel('effective barrier')
+        if rank == 0:
+            print(f'Realization {r+1}...')
 
-    ax2.xlabel('number of paths')
-    ax2.ylabel('permeability per path')
-    plt.show()
+        for i,N in enumerate(my_paths):
+            effective_barriers[i], permeabilities[i] = calculate_barriers_permeability(N)
+
+        # create big arrays for Gather
+        all_barriers = np.zeros(sum(count))
+        all_permeabilities = np.zeros(sum(count))
+
+        # gather all the barriers and permeabilities back to master for plotting
+        comm.Gatherv(effective_barriers, [all_barriers, count, displ, MPI.DOUBLE], root=0)
+        comm.Gatherv(permeabilities, [all_permeabilities, count, displ, MPI.DOUBLE], root=0)
+
+        if rank == 0:
+            # save for average across realizations 
+            avg_barrier += all_barriers + R*T*np.log(n_paths)
+            avg_permeability += all_permeabilities / n_paths
+
+            ax1.plot(n_paths, all_barriers + R*T*np.log(n_paths), alpha=0.1, c='k')
+            ax2.plot(n_paths, all_permeabilities / n_paths, alpha=0.1, c='k')
+        
+
+    if rank == 0:
+        ax1.plot(n_paths, avg_barrier / realizations, c='r')
+        ax2.plot(n_paths, avg_permeability / realizations, c='r')
+
+        ax1.set_xlabel('number of paths')
+        ax1.set_ylabel('effective barrier')
+        ax2.set_xlabel('number of paths')
+        ax2.set_ylabel('permeability per path')
+        plt.show()
