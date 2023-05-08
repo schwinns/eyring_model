fixed units issue and testing why dS = 0

eyring_model.py
@@ -162,7 +162,7 @@ class Path:
             # generate enthalpy and entropy distributions
             multi_norm = rng.multivariate_normal(mean=dist_params['mu'], cov=dist_params['cov'], size=self.n_jumps)
             self.enthalpic_barriers = multi_norm[:,0]
-            self.entropic_barriers = -multi_norm[:,1] / self.T
+            self.entropic_barriers = multi_norm[:,1]
             self.membrane_barriers = self.enthalpic_barriers - self.T*self.entropic_barriers # calculate dG from dH and dS
 
             # plt.hist(self.enthalpic_barriers, color='r', alpha=0.5, edgecolor='k')
@@ -403,20 +403,3 @@ class Path:
 if __name__ == '__main__':
 
     T = 300
-    lam = 10
-    n_jumps = 50
-    multi = True
-    dist = 'normal'
-    dist_params = {'mu' : np.array([14,8]),
-                   'cov': np.array([[14/3,0],
-                                    [0,8/3]])}
-
-    p = Path(T=T, n_jumps=n_jumps, lam=lam)
-    dG, dH = p.generate_membrane_barriers(dist=dist, dist_params=dist_params, multi=multi)
-
-    fig, ax = plt.subplots(2,1, figsize=(6,10), sharex=True)
-    ax[0].hist(dG, bins=50, edgecolor='k', alpha=0.75)
-    ax[0].set_xlabel('dG')
-    ax[1].hist(dH, bins=50, edgecolor='k', alpha=0.75)
-    ax[1].set_xlabel('dH')
-    plt.show()

run_analyses.py
@@ -21,7 +21,7 @@ parallel_pores = False
 compare_effective_barriers = False
 estimate_dH_dS_barrier_distributions = False
 estimate_dH_dS_jump_distributions = False
-estimate_dH_dS_spread = False
+estimate_dH_dS_spread = True
 compare_jump_lengths = False
 plot_paths = False
 test_path_convergence = False
@@ -463,9 +463,9 @@ if estimate_dH_dS_spread:
     print('\nMULTIVARIATE NORMAL')
 
     params = {
-        'mu'  : np.array([4.5, 6]),
-        'cov' : np.array([[0.001**2,0],
-                          [0,0.001**2]])
+        'mu'  : np.array([4.5, -6/300]),
+        'cov' : np.array([[0.0001**2,0],
+                          [0,0.0001**2]])
     }
 
     dist = 'normal'
@@ -477,23 +477,31 @@ if estimate_dH_dS_spread:
             model.add_Path(n_jumps=200, lam=10)
             model.paths[n].generate_membrane_barriers(dist=dist, multi=multi, dist_params=params)
 
-        P[i] = model.calculate_permeability()
+        P[i] = model.calculate_permeability() / 60 / 60 / 1000 * 10**9 * 10
         dG_eff[i] = model.calculate_effective_barrier()
         lam = model.get_lambda()
         delta = np.array(model.deltas).mean()
         X[i] = 1 / T
         Y[i] = np.log(P[i]*h*delta / (kB*T*lam**2))
 
-    m, b = np.polyfit(X,Y,1)
+    # m, b = np.polyfit(X,Y,1)
+    A = np.vstack([X, np.ones(len(X))]).T
+    m, b = np.linalg.lstsq(A,Y, rcond=None)[0]
     print(f'dH_eff : {-m*R}')
     print(f'dS_eff : {b*R} or -T dS_eff at 300 K: {-300*b*R}')
     print(f'dG_eff at 300 K: {dG_eff.mean()}')
+
+    print()
+    a, b, c = np.polyfit(temps, dG_eff, deg=2)
+    print(f'dH_m : {b/R + np.log(n_paths)}')
+    print(f'dS_m : {-a/R} or -T dS_m at 300 K: {a/R*300}')
+    print(f'dG_eff at 300 K: {dG_eff.mean()}')
+    print('\n', a, b, c)
 
     df1 = pd.DataFrame()
     df1['distribution'] = ['multi-variate normal']*len(temps)
     df1['temperature'] = temps
     df1['permeability'] = P
-    df1['log permeability'] = np.log(P)
     df1['effective free energy'] = dG_eff
     df1['1/T'] = X
     df1['ln(P h del / kB T lam^2)'] = Y
@@ -542,11 +550,8 @@ if estimate_dH_dS_spread:
     plt.figure()
     sns.scatterplot(data=df1, x='temperature', y='permeability', hue='distribution')
 
-    plt.figure()
-    sns.scatterplot(data=df1, x='temperature', y='log permeability')
-
-    plt.figure()
-    sns.scatterplot(data=df1, x='temperature', y='effective free energy', hue='distribution')
+    sns.lmplot(data=df1, x='temperature', y='effective free energy', hue='distribution',
+               scatter_kws={'alpha':0.75, 'edgecolor':'black'})
 
     plt.show()
 